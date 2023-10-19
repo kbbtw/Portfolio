10887d557f3b636c931289f93324c10e7c8a3683
kbbtw <138120811+kbbtw@users.noreply.github.com>
2023-10-19 15:22:33 +0100

    Updated js file
    Removed temporary away alert

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-alert("Hi, Thanks for visting my website! Some features and content may be unavailable as I continue to work on them. I'm currently on holiday so check back next week for some improvements! ")
-
-
-
-
-
 const picBtn = document.querySelector("#picture-tab");
 const content = document.querySelector("#about-content")
 
